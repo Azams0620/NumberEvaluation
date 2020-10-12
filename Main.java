1c0455b022a2c3af91410be82a79b4ef8facf615
Azams0620 <sayeed.azam@patelhs.org>
2020-10-12 12:14:47 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; 
+
+public class Main {
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+    System.out.print("What is your name?");
+    
+    
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  }
+  
+}
