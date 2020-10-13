bcf223e05f72e962c4f3f6cb0a395f013735d568
Azams0620 <sayeed.azam@patelhs.org>
2020-10-13 17:43:46 +0000

    1. Imported Scanner 2. Input for name 3. Input for number 4. If statement check for required range 5. Input for guess of favNum 6. If statement for

--- Main.java
+++ Main.java
@@ -11,29 +11,68 @@ public class Main {
     System.out.println("Hi " + name + ", I am Number Bot");
 
     //user input for number
-    System.out.println("Hey " + name + ", pick a whole number 1 through 100. MAKE SURE IT IS BETWEEN THOSE NUMBERS");
+    System.out.println("Hey " + name + ", pick a whole number 1 through 10. MAKE SURE IT IS BETWEEN THOSE NUMBERS");
     int num = scan.nextInt();
 
 
     //Response if number is not between 1 and 100
-    if(num< 1 || num > 100){ // If they chose a number not 1-100
-      num = 100;
-      System.out.print("Sorry, you didn't listen to me, and chose " + num +", so I made your number 100!");
+    if(num< 1 || num > 10){ // If they chose a number not 1-10
+      num = 10;
+      System.out.println("Sorry, you didn't listen to me, and chose " + num +", so I made your number 10!");
     }
-    else{// This is for is they chose a number 1-100
-      System.out.print("Good job! You chose a number between 1 and 100: " + num);
+    else{// This is for is they chose a number 1-10
+      System.out.println("Good job! You chose a number between 1 and 10: " + num);
     }
 
 
-    int favNum = 77;
-    System.out.println(name + ", try to guess my favorite whole number");
+    int favNum = 7;
+    System.out.println(name + ", try to guess my favorite whole number between 1 and 10");
     int guessOne = scan.nextInt();
 
     if(guessOne != favNum){
-      System.out.println("Nope, that's not it")
-       
+      System.out.println("Nope, that's not it. My number was actually 7");}
+    else{
+      System.out.println("Yeah, that's right!");
     }
      
+    int sum = num + favNum;
+    System.out.println("I wonder what the sum of both our numbers are. Oh! it's " + sum);
+
+    
+    System.out.println("Let's see how many times our sum can go into 100");
+    int sumA = 0;
+    int part = 1;
+    while(sumA<= (100-sumA)){
+      sumA = sumA + sum;
+      System.out.println(part);
+      part++;
+    }
+    
+    
+    
+    
+    System.out.println("Hey, choose a number with a decimal between 1 and 10");
+    double numDoub = scan.nextDouble();
+    if(numDoub > 10.0 && numDoub >= 10.001){
+      System.out.println("Sorry, you didn't listen to me, and chose " + numDoub +", so I made your number 10.0!");
+      numDoub = 10.0;
+    }
+    else 
+    if(numDoub > 10.0 ||numDoub < 1.0){
+      System.out.println("Sorry, you didn't listen to me, and chose " + numDoub +", so I made your number 10.0!");
+      numDoub = 10.0;}
+    else{
+      System.out.println("Cool! the number you chose was " + numDoub);
+    }
+
+    
+
+
+
+
+
+
+
     
   
 
